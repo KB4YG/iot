7cc792305f15cb3d53b6702382ce5ce67ab1e1d4
Tyson Fairhurst <tyson.fairhurst@gmail.com>
2022-04-08 11:34:00 -0700

    Update camera.py

--- src/hardware/camera.py
+++ src/hardware/camera.py
@@ -1,10 +1,34 @@
 from time import sleep
 import os
 import time
-
+from picamera import PiCamera
+from fractions import Fraction
+import datetime
 class CameraFileNotFoundError(FileNotFoundError):
     '''Camera file not found after attempting to take picture'''
 
+
+def takepicDark(exposureTime):
+    
+    cur_time = datetime.datetime.now()
+    stub = cur_time.strftime("%Y%m%d%H%M_low")
+
+    camera = PiCamera(framerate=Fraction(1,6))
+
+    # You can change these as needed. Six seconds (6000000)
+    # is the max for shutter speed and 800 is the max for ISO.
+    camera.shutter_speed = 1750000
+    camera.iso = 800
+
+    time.sleep(30)
+    camera.exposure_mode = 'off'
+
+    outfile = "%s.jpg" % (stub)
+    camera.capture(outfile)
+
+    camera.close()
+ 
+    
 def takepic(exposureTime): 
     fileName = "img_" + str(time.time()) + ".jpg"
     filePath = "./img/" + fileName
