49d92ef3a0440afada0eb16453372c18b4355f97
Phillip Bindeman <bindeman@me.com>
2022-04-22 17:51:39 -0700

    Update camera.py
    revert libcamera

--- src/hardware/camera.py
+++ src/hardware/camera.py
@@ -1,45 +1,18 @@
 from time import sleep
 import os
 import time
-from picamera import PiCamera
-from fractions import Fraction
-import datetime
+
 class CameraFileNotFoundError(FileNotFoundError):
     '''Camera file not found after attempting to take picture'''
 
-
-def takepicDark(exposureTime):
-    
-    cur_time = datetime.datetime.now()
-    stub = cur_time.strftime("%Y%m%d%H%M_low")
-
-    camera = PiCamera(framerate=Fraction(1,6))
-
-    # You can change these as needed. Six seconds (6000000)
-    # is the max for shutter speed and 800 is the max for ISO.
-    camera.shutter_speed = 1750000
-    camera.iso = 800
-
-    time.sleep(30)
-    camera.exposure_mode = 'off'
-
-    outfile = "%s.jpg" % (stub)
-    camera.capture(outfile)
-
-    camera.close()
- 
-    
 def takepic(exposureTime): 
     fileName = "img_" + str(time.time()) + ".jpg"
     filePath = "./img/" + fileName
-    # command = "libcamera-still -t " + str(exposureTime) + " -o "  + filePath
+    command = "libcamera-still -t " + str(exposureTime) + " -o "  + filePath
     print(command)
-    # os.system(command)
-    # sleep(10)
-    camera = PiCamera()
-    # Camera warm-up time
-    sleep(2)
-    camera.capture(filePath)
+    os.system(command)
+    sleep(10)
+    
     f = open(filePath, "r")
     if f:
         print("File opened successfully")
@@ -61,4 +34,3 @@ def cameraHandler(exposureTime = 1000):
     #     print(err.args)
     #     print("Attempting to try to take picture again")
     #     fileName = takepic(exposureTime)
-
